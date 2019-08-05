e18ecdbbf39ecbd55265d9a470de5a2fbe350076
Jony <maybe-one-day-ubermensch@protonmail.com>
2019-08-05 08:17:30 -0700

    f/l.d/xdg_runtime_dir: removed extraneous test within square brackets

--- fish/.config/fish/login.d/set_xdg_runtime_dir.fish
+++ fish/.config/fish/login.d/set_xdg_runtime_dir.fish
@@ -3,7 +3,7 @@
 if [ -z "$XDG_RUNTIME_DIR" ]
     set -gx XDG_RUNTIME_DIR /tmp/$USER-runtime-dir
     
-    if [ test -d "$XDG_RUNTIME_DIR" ]
+    if [ -d "$XDG_RUNTIME_DIR" ]
         mkdir "$XDG_RUNTIME_DIR"
         chmod 0700 "$XDG_RUNTIME_DIR"
     end
