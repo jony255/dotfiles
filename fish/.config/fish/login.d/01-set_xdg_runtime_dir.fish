8ce88746d2469df9a0860d219e6a63b25e49c22c
Jony <maybe-one-day-ubermensch@protonmail.com>
2019-08-05 08:50:28 -0700

    fish/login.d/set_xdg_runtime_dir.fish: added a missing '!' to dir check

--- fish/.config/fish/login.d/01-set_xdg_runtime_dir.fish
+++ fish/.config/fish/login.d/01-set_xdg_runtime_dir.fish
@@ -1,8 +1,8 @@
 if [ -z "$XDG_RUNTIME_DIR" ]
     set -gx XDG_RUNTIME_DIR /tmp/$USER-runtime-dir
     
-    if [ -d "$XDG_RUNTIME_DIR" ]
-        mkdir "$XDG_RUNTIME_DIR"
+    if [ ! -d "$XDG_RUNTIME_DIR" ]
+        mkdir -pv "$XDG_RUNTIME_DIR"
         chmod 0700 "$XDG_RUNTIME_DIR"
     end
 end
