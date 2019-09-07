36d167673a7a0c3082c036d8c69a59a68a5fa3af
Jony <maybe-one-day-ubermensch@protonmail.com>
2019-09-06 23:32:58 -0700

    fish/config.fish: put .login_d_fish symlink in a variable

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,10 @@
 #!/usr/bin/env fish
 
 function main
+    set LOGIN_DIR "$HOME/.login_d_fish"
+
     if status is-login
-        for login_d_script in (find -L "$HOME/.login_d_fish" -name '*.fish' -type f)
+        for login_d_script in (find -L $LOGIN_DIR -name '*.fish' -type f)
             source $login_d_script
         end
 
