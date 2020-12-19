cf4d86b633c7cd4b880b835827ab877f5d57bcc4
Jony <maybe-one-day-ubermensch@protonmail.com>
2020-12-19 13:22:03 -0800

    refactor volume script to posix sh

---
+++ sway/.config/sway/scripts/volume_control.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# TODO: Implement error checking for inputs.
+
+volume_control() {
+    VOLUME_DIRECTION="$1"
+    VOLUME_DELTA="$2"
+
+    VOLUME_CMD='amixer'
+    VOLUME_FLAGS='set Master'
+
+    if [ "$VOLUME_DIRECTION" = "raise" ]; then
+        $VOLUME_CMD $VOLUME_FLAGS ${VOLUME_DELTA}%+
+    elif [ "$VOLUME_DIRECTION" = "lower" ]; then
+        $VOLUME_CMD $VOLUME_FLAGS ${VOLUME_DELTA}%-
+    elif [ "$VOLUME_DIRECTION" = "toggle" ]; then
+        $VOLUME_CMD $VOLUME_FLAGS toggle
+    fi
+}
+
+volume_control $@
