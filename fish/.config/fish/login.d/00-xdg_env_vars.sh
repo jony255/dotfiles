ba5a2329152a848fa86f10b618749631d24fb5a8
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-03-22 20:50:25 +0000

    add `export XDG_CURRENT_DESKTOP='sway'` for xdpw

--- fish/.config/fish/login.d/00-xdg_env_vars.sh
+++ fish/.config/fish/login.d/00-xdg_env_vars.sh
@@ -3,3 +3,5 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share:$XDG_DATA_HOME/flatpak/exports/share"
 export XDG_CONFIG_DIRS='/etc/xdg'
+
+export XDG_CURRENT_DESKTOP='sway'
