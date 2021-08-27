706f1a981649e6cc75f6e86462d88b21e7ceb866
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-08-27 10:56:57 -0700

    setup `pylsp`

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -90,6 +90,11 @@ lsp_config["rust_analyzer"].setup({
     on_attach=on_attach,
 })
 
+lsp_config["pylsp"].setup({
+    on_attach=on_attach,
+})
+
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         -- This will disable virtual text, like doing:
