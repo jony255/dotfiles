ae5f3e98b196c712a760616336304e0f578f951e
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-09-29 13:42:55 -0700

    use the recommended way of using `nvim` as `MANPAGER`

--- fish/.config/fish/functions/fman.fish
+++ fish/.config/fish/functions/fman.fish
@@ -1,5 +1,5 @@
 function fman
     set -lx MANPATH "$__fish_data_dir/man"
 
-    command man $argv 1>/dev/null && command man $argv | nvim -c 'set ft=man'
+    man $argv
 end
--- fish/.config/fish/functions/man.fish
+++
@@ -1,3 +0,0 @@
-function man
-    command man $argv 1>/dev/null && command man $argv | nvim -c 'set ft=man'
-end
