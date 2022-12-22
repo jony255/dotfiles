37e2adf2eeadf95864b2e7fa4f9ac19e0cf0f5a6
Jony <maybe-one-day-ubermensch@protonmail.com>
2022-12-22 14:22:02 -0800

    nvim/lua/config/gitsigns.lua: prioritze gutter signs
    I don't link it when 'diagnostics' signs are prioritzed over 'gitsigns'.
    I need to find a better way to order them but I'm good with this for
    now.

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -53,7 +53,7 @@ M.setup = function()
             ignore_whitespace = false,
         },
         current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-        sign_priority = 6,
+        sign_priority = 50,
         update_debounce = 100,
         status_formatter = nil, -- Use default
         max_file_length = 40000, -- Disable if file is longer than this (in lines)
