c5b6c1023a77d3620b38356ddfe7349356dd91f4
Jony <maybe-one-day-ubermensch@protonmail.com>
2023-06-19 19:19:34 -0700

    nvim/lua/cmp/dictionary.lua: use proper `cmp_dictionary::setup()`
    'dic' is no longer  used.

--- nvim/lua/config/cmp/dictionary.lua
+++ nvim/lua/config/cmp/dictionary.lua
@@ -1,16 +1,20 @@
 local M = {}
 
-function M.setup()
-    require('cmp_dictionary').setup {
-        dic = {
-            ['*'] = { '/usr/share/dict/words' },
-        },
+local cmp_dictionary = require('cmp_dictionary')
 
+function M.setup()
+    cmp_dictionary.setup {
         exact = 2,
         first_case_insensitive = true,
         async = true,
         capacity = 5,
         debug = false,
+    }
+
+    cmp_dictionary.switcher {
+        spelllang = {
+            en = '/usr/share/dict/words',
+        },
     }
 end
 
