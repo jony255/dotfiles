48ae049470ff7b2909a90bef82ebc3b549c857ef
Jony <maybe-one-day-ubermensch@protonmail.com>
2023-07-01 11:42:42 -0700

    sway/scripts/volume_control.sh: use `wpctl` directly for volume
    I'm already all in on 'wireplumber' anyways.

--- sway/scripts/volume_control.sh
+++ sway/scripts/volume_control.sh
@@ -6,8 +6,8 @@ volume_control() {
     VOLUME_DIRECTION="$1"
     VOLUME_DELTA="$2"
 
-    VOLUME_CMD='amixer'
-    VOLUME_FLAGS='-D pipewire set Master'
+    VOLUME_CMD='wpctl'
+    VOLUME_FLAGS='set-volume @DEFAULT_AUDIO_SINK@'
 
     if [ "$VOLUME_DIRECTION" = "raise" ]; then
         $VOLUME_CMD $VOLUME_FLAGS ${VOLUME_DELTA}%+
