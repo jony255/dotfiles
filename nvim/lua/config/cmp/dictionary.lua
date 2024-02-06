1603ebb3e1614465da347668d63ab8a0d44b8b08
Jony <maybe-one-day-ubermensch@protonmail.com>
2024-02-05 20:37:30 -0800

    nvim: cmp: dictionary: use updated config options

--- nvim/lua/config/cmp/dictionary.lua
+++ nvim/lua/config/cmp/dictionary.lua
@@ -4,16 +4,12 @@ local cmp_dictionary = require('cmp_dictionary')
 
 function M.setup()
     cmp_dictionary.setup {
-        exact = 2,
+        exact_length = 2,
         first_case_insensitive = true,
         async = true,
-        capacity = 5,
         debug = false,
-    }
-
-    cmp_dictionary.switcher {
-        spelllang = {
-            en = '/usr/share/dict/words',
+        paths = {
+            '/usr/share/dict/words',
         },
     }
 end
