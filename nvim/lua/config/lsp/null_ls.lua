6516982a1f66c29b00f89432732ca8b768684a5d
Jony <maybe-one-day-ubermensch@protonmail.com>
2024-02-05 20:51:24 -0800

    nvim: lua: null_ls: add linters/formatters for cmake

--- nvim/lua/config/lsp/null_ls.lua
+++ nvim/lua/config/lsp/null_ls.lua
@@ -8,8 +8,12 @@ function M.setup(on_attach)
         on_attach = on_attach,
         sources = {
             builtins.formatting.stylua,
+            builtins.formatting.cmake_format,
+
+            builtins.diagnostics.cmake_lint,
             builtins.diagnostics.luacheck,
             builtins.diagnostics.shellcheck,
+
             builtins.code_actions.shellcheck,
         },
     }
